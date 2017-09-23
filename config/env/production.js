36ff0c2417fdbc559d8078230c19d91cb03ee1ff
minddwicha <wiracha.mindd@g.swu.ac.th>
2017-09-23 12:06:25 +0700

    add file and edit

---
+++ config/env/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+       // mongoUri: 'mongodb://localhost/myFirstNode',
+       // or use 
+       mongoUri: 'mongodb://admin:password@localhost:27017/testdb1',
+       debug: true,
+       sessionSecret: 'dev_secret_key'
+}
